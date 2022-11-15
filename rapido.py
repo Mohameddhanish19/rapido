012075773ef58cb1c0929b48b0cce8adad879608
Mohameddhanish19 <113160286+Mohameddhanish19@users.noreply.github.com>
2022-11-15 14:52:41 +0530

    commit10

--- rapido.py
+++ rapido.py
@@ -1,4 +1,4 @@
-thisset = {"apple", "banana", "cherry", "grape", "orange", "guva", "kiwi", "musmbi"}
+thisset = {"apple", "banana", "cherry", "grape", "orange", "guva", "kiwi", "musmbi", "sapota"}
 
 thisset.remove("apple")
 
